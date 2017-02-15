2fff25bdb6e31065fda4fa9cc333706cc0c8f886
M0rkeleb <wolfwood.sjc@gmail.com>
2017-02-14 23:49:16 -0500

    Helper funcs and const iterators
    Wrote helper functions (since we're going to have 4 types of iterators)
    and implemented const iterator (as a separate non-templated class to
    make sure I could include ref to array as a member)

--- array_2d_iterator.hpp
+++ array_2d_iterator.hpp
@@ -7,127 +7,199 @@
 #include <algorithm>
 #include <type_traits>
 
+template<typename T>
+using array_2d = boost::multi_array<T, 2>;
+
+using size_t_pair = std::pair<std::size_t, std::size_t>;
+
+inline void move_fwd(char axis, size_t_pair& locat) {
+	switch (axis)
+	{
+	case 'h':
+		locat.second++; break;
+	case 'v':
+		locat.first++; break;
+	case 'd':
+		locat.first++; locat.second++; break;
+	case 'a':
+		locat.second--; locat.first++; break;
+	default:
+		break;
+	}
+}
+
+inline void move_back(char axis, size_t_pair& locat) {
+	switch (axis)
+	{
+	case 'h':
+		locat.second--; break;
+	case 'v':
+		locat.first--; break;
+	case 'd':
+		locat.first--; locat.second--; break;
+	case 'a':
+		locat.second++; locat.first--; break;
+	default:
+		break;
+	}
+}
+
+inline bool equal_loc(char axis, size_t_pair loc_1, size_t_pair loc_2, std::size_t width) {
+	//used to compare iterators
+	//Trying to compare iterators like these gets very fuzzy
+	//Main intention will be to have a 2dbegin and 2dend at opposite corners and 
+	//to provide the same kind of boundaries begin and end do for regular containers
+	//Aside from that this is a pretty useless function
+	//The resulting operation is not commutative or associative 
+	switch (axis)
+	{
+	case 'h':
+		return (loc_1.second == loc_2.second);
+	case 'v':
+		return (loc_1.first == loc_2.first);
+	case 'd':
+		return (loc_1.second == loc_2.second || loc_1.first == loc_2.first);
+	case 'a':
+		return (loc_1.second + loc_2.second == width + 1 || loc_1.first == loc_2.first);
+	default:
+		return true;
+	}
+}
+
 template <class T>
 class array_2d_iterator : public boost::iterator_facade<array_2d_iterator<T>,T,boost::random_access_traversal_tag>
 {
 public:
-	array_2d_iterator() : m_ptr(nullptr), m_dir('h'), a_width(0) { a_loc.first = 0; a_loc.second = 0; }
-	array_2d_iterator(T *p) : m_ptr(p), m_dir('h'), a_width(0) { a_loc.first = 0; a_loc.second = 0; }
-	array_2d_iterator(T *p, char d, std::size_t w, std::size_t i = 0, std::size_t j = 0) : m_ptr(p), m_dir(d), a_width(w) { a_loc.first = i; a_loc.second = j; }
+	array_2d_iterator(array_2d<T>& source, std::size_t i, std::size_t j) : m_source(source), m_axis('h') { a_loc.first = i; a_loc.second = j; }
+	array_2d_iterator(array_2d<T>& source, std::size_t i, std::size_t j, char d) : m_source(source), m_axis(d) { a_loc.first = i; a_loc.second = j; }
 	template <class U, std::enable_if_t<std::is_convertible<U*, T*>::value,int> = 0>
-	array_2d_iterator(array_2d_iterator<U> const& other) : m_ptr(other.m_ptr), m_dir(other.m_dir), a_width(other.a_width), a_loc(other.a_loc) {}
+	array_2d_iterator(array_2d_iterator<U> const& other) : m_source(other.m_source), m_axis(other.m_axis), a_loc(other.a_loc) {}
 private:
-	T *m_ptr;
-	char m_dir;
+	array_2d<T>& m_source;
+	char m_axis;
 	// Direction of this iterator - four possibilities.
 	// h = horizontal
 	// v = vertical
 	// d = descending (diagonally)
 	// a = ascending (diagonally)
-	std::pair<std::size_t, std::size_t> a_loc; //location in the array, needed to be able to compare iterators on different slices and for difference
-	std::size_t a_width; //width of the 2d array, needed to traverse with non-horizontal iterators
+	std::pair<std::size_t, std::size_t> a_loc; 
+	//location in the array - but starting at 1,1 to be able to be consistent with reverse iterator so rend can have index 0,0
+	//needed to be able to compare iterators on different slices and for difference
+	std::size_t a_width() const { return m_source.shape()[1]; } //width of the 2d array, helpful to traverse with non-horizontal iterators
 	friend class boost::iterator_core_access;
 	template <class> friend class array_2d_iterator;
-	void increment() {
-		switch (m_dir)
-		{
-		case 'h':
-			m_ptr++; a_loc.second++; break;
-		case 'v':
-			m_ptr += a_width; a_loc.first++; break;
-		case 'd':
-			m_ptr += a_width; m_ptr++; 
-			a_loc.first++; a_loc.second++; break;
-		case 'a':
-			m_ptr += a_width; m_ptr--;
-			a_loc.second--; a_loc.first++; break;
-		default:
-			break;
-		}
-	}
-	void decrement() {
-		switch (m_dir)
-		{
-		case 'h':
-			m_ptr--; a_loc.second--; break;
-		case 'v':
-			m_ptr -= a_width; a_loc.first--; break;
-		case 'd':
-			m_ptr -= a_width; m_ptr--;
-			a_loc.first--; a_loc.second--; break;
-		case 'a':
-			m_ptr -= a_width; m_ptr++;
-			a_loc.second++; a_loc.first--; break;
-		default:
-			break;
-		}
-	}
-	template <class U>
-	bool equal(array_2d_iterator<U> const& other) const {
-		//Trying to compare iterators like these gets very fuzzy
-		//Main intention will be to have a 2dbegin and 2dend at opposite corners and 
-		//let x == 2dbegin/2dend mean x cannot be decremented/incremented anymore
-		//Aside from that this is a pretty useless function
-		//The resulting operation is not commutative or associative 
-		switch (m_dir)
-		{
-		case 'h':
-			return (a_loc.second == other.a_loc.second);
-		case 'v':
-			return (a_loc.first == other.a_loc.first);
-		case 'd':
-			return (a_loc.second == other.a_loc.second || a_loc.first == other.a_loc.first);
-		case 'a':
-			return (a_loc.second + other.a_loc.second + 1 == a_width || a_loc.first == other.a_loc.first);
-		default:
-			return true;
-		}
+	void increment() { move_fwd(m_axis, a_loc); }
+	void decrement() { move_back(m_axis, a_loc); }
+	bool equal(array_2d_iterator<T> const& other) const {
+		if (m_source != other.m_source) { return false; }
+		return equal_loc(m_axis, a_loc, other.a_loc, this->a_width());
 	}
 	void advance(ptrdiff_t n) {
 		for (ptrdiff_t i = 0; i < n; i++) { increment(); }
 	}
-	template <class U>
-	ptrdiff_t distance_to(array_2d_iterator<U> const& other) const {
+	ptrdiff_t distance_to(array_2d_iterator<T> const& other) const {
 		//Another fuzzy comparison!  This time x.distance_to(z) == n means x.advance(n).equal(z) == true
 		//If x and z have the same direction this is straightforward
 		//But in general also does not behave as expected with math (e.g. x.distance_to(z) might not be negative of z.distance_to(x) if they have different directions
-		if (m_dir == 'h') { return static_cast<ptrdiff_t>(other.a_loc.second - a_loc.second); }
-		return static_cast<ptrdiff_t>(other.a_loc.first - a_loc.first);
+		if (m_axis == 'h') { return static_cast<ptrdiff_t>(other.a_loc.second) - static_cast<ptrdiff_t>(a_loc.second); }
+		return static_cast<ptrdiff_t>(other.a_loc.first) - static_cast<ptrdiff_t>(a_loc.first);
 	}
-	T& dereference() const { return *(this->m_ptr); }
+	T& dereference() const { return m_source[a_loc.first - 1][a_loc.second - 1]; }
 };
 
-template<typename T>
-using array_2d = boost::multi_array<T, 2>;
+//Convenience functions for begin, end, and arbitrary index iterator creators from a given 2d_array
 
 template<typename T>
 array_2d_iterator<T> two_d_begin(array_2d<T> &source, char dir = 'h') {
-	return array_2d_iterator<T>(source.origin(), dir, source.shape()[1], 0, 0);
+	return array_2d_iterator<T>(source, 1, 1, dir);
 }
 
 template<typename T>
 array_2d_iterator<T> two_d_end(array_2d<T> &source, char dir = 'h') {
-	return array_2d_iterator<T>(source.origin()+source.num_elements()-1, dir, source.shape()[1], source.shape()[0]-1, source.shape()[1]-1);
+	return array_2d_iterator<T>(source, source.shape()[0] + 1, source.shape()[1] + 1, dir);
 }
 
 template<typename T>
-array_2d_iterator<T> iter_from_coord(array_2d<T> &source, int i, int j, char dir = 'h') {
-	auto width = source.shape()[1];
-	return array_2d_iterator<T>(source.origin() + i*width + j, dir, width, i, j);
+array_2d_iterator<T> iter_from_coord(array_2d<T> &source, std::size_t i, std::size_t j, char dir = 'h') {
+	//coord relative to usual array indexing (assuming index starts at 0,0 .. could generalize later
+	return array_2d_iterator<T>(source, i + 1, j + 1, dir);
+}
+
+template <class T>
+class const_array_2d_iterator : public boost::iterator_facade<const_array_2d_iterator<T>, T const, boost::random_access_traversal_tag>
+{
+public:
+	const_array_2d_iterator(array_2d<T> const& source, std::size_t i, std::size_t j) : m_source(source), m_axis('h') { a_loc.first = i; a_loc.second = j; }
+	const_array_2d_iterator(array_2d<T> const& source, std::size_t i, std::size_t j, char d) : m_source(source), m_axis(d) { a_loc.first = i; a_loc.second = j; }
+	template <class U, std::enable_if_t<std::is_convertible<U*, T*>::value, int> = 0>
+	const_array_2d_iterator(const_array_2d_iterator<U> const& other) : m_source(other.m_source), m_axis(other.m_axis), a_loc(other.a_loc) {}
+private:
+	array_2d<T> const& m_source;
+	char m_axis;
+	// Direction of this iterator - four possibilities.
+	// h = horizontal
+	// v = vertical
+	// d = descending (diagonally)
+	// a = ascending (diagonally)
+	std::pair<std::size_t, std::size_t> a_loc;
+	//location in the array - but starting at 1,1 to be able to be consistent with reverse iterator so rend can have index 0,0
+	//needed to be able to compare iterators on different slices and for difference
+	std::size_t a_width() const { return m_source.shape()[1]; } //width of the 2d array, helpful to traverse with non-horizontal iterators
+	friend class boost::iterator_core_access;
+	template <class> friend class array_2d_iterator;
+	void increment() { move_fwd(m_axis, a_loc); }
+	void decrement() { move_back(m_axis, a_loc); }
+	bool equal(const_array_2d_iterator<T> const& other) const {
+		if (m_source != other.m_source) { return false; }
+		return equal_loc(m_axis, a_loc, other.a_loc, this->a_width());
+	}
+	void advance(ptrdiff_t n) {
+		for (ptrdiff_t i = 0; i < n; i++) { increment(); }
+	}
+	ptrdiff_t distance_to(const_array_2d_iterator<T> const& other) const {
+		//Another fuzzy comparison!  This time x.distance_to(z) == n means x.advance(n).equal(z) == true
+		//If x and z have the same direction this is straightforward
+		//But in general also does not behave as expected with math 
+		//(e.g. x.distance_to(z) might not be negative of z.distance_to(x) if they have different directions
+		if (m_axis == 'h') { return static_cast<ptrdiff_t>(other.a_loc.second) - static_cast<ptrdiff_t>(a_loc.second); }
+		return static_cast<ptrdiff_t>(other.a_loc.first) - static_cast<ptrdiff_t>(a_loc.first);
+	}
+	T const& dereference() const { return m_source[a_loc.first - 1][a_loc.second - 1]; }
+};
+
+
+
+
+//Create const interators .. I hope
+
+template<typename T>
+const_array_2d_iterator<T> two_d_begin(array_2d<T> const &source, char dir = 'h') {
+	return const_array_2d_iterator<T>(source, 1, 1, dir);
 }
 
 template<typename T>
-array_2d_iterator<T const> ctwo_d_begin(array_2d<T> const &source, char dir = 'h') {
-	return array_2d_iterator<T const>(source.origin(), dir, source.shape()[1], 0, 0);
+const_array_2d_iterator<T> two_d_end(array_2d<T> const &source, char dir = 'h') {
+	return const_array_2d_iterator<T>(source, source.shape()[0] + 1, source.shape()[1] + 1, dir);
 }
 
 template<typename T>
-array_2d_iterator<T const> ctwo_d_end(array_2d<T> const &source, char dir = 'h') {
-	return array_2d_iterator<T const>(source.origin() + source.num_elements() - 1, dir, source.shape()[1], source.shape()[0] - 1, source.shape()[1] - 1);
+const_array_2d_iterator<T> iter_from_coord(array_2d<T> const &source, std::size_t i, std::size_t j, char dir = 'h') {
+	//coord relative to usual array indexing (assuming index starts at 0,0 .. could generalize later
+	return const_array_2d_iterator<T>(source, i + 1, j + 1, dir);
 }
 
 template<typename T>
-array_2d_iterator<T const> citer_from_coord(array_2d<T> const &source, int i, int j, char dir = 'h') {
-	auto width = source.shape()[1];
-	return array_2d_iterator<T const>(source.origin() + i*width + j, dir, width, i, j);
+const_array_2d_iterator<T> ctwo_d_begin(array_2d<T> const &source, char dir = 'h') {
+	return two_d_begin(source, dir);
+}
+
+template<typename T>
+const_array_2d_iterator<T> ctwo_d_end(array_2d<T> const &source, char dir = 'h') {
+	return two_d_end(source, dir);
+}
+
+template<typename T>
+const_array_2d_iterator<T> citer_from_coord(array_2d<T> const &source, std::size_t i, std::size_t j, char dir = 'h') {
+	//coord relative to usual array indexing (assuming index starts at 0,0 .. could generalize later
+	return iter_from_coord(source, i, j, dir);
 }
